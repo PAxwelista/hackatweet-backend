e5201fcb475427f8433140570412db5fb9b0d2fd
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2024-12-24 00:17:11 +0100

    add env variable

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-require('dotenv').config()
-require("./models/connection.js")
+require("dotenv").config();
+require("./models/connection.js");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const tweetRouter = require('./routes/tweets');
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const tweetRouter = require("./routes/tweets");
 
 var app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tweets', tweetRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/tweets", tweetRouter);
 
 module.exports = app;
