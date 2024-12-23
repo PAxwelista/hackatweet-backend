71989bcb91f385f1f1eca4b0794f3320f7716959
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2024-12-24 00:27:30 +0100

    change Uppercase file names

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Tweet = require("../models/tweet");
+const Tweet = require("../models/tweets");
 const User = require("../models/users");
 
 router.get("/", async (req, res) => {
